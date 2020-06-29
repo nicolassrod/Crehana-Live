0273029cbd11012664ad2884f970431a05d5a862
Nicolás A. Rodríguez <nrq5510@gmail.com>
2020-06-29 10:25:15 -0500

    Initial Commit

---
+++ Crehana Live/Crehana_LiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crehana_LiveApp.swift
+//  Crehana Live
+//
+//  Created by Nicolás A. Rodríguez on 6/29/20.
+//
+
+import SwiftUI
+
+@main
+struct Crehana_LiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
